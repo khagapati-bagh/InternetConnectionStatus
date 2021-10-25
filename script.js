3902581b4a490a541ca51f411816b7f28c3cb67e
Khagapati Bagh <30410947+khagapati-bagh@users.noreply.github.com>
2021-10-25 09:26:24 +0530

    Revert "Add style.css"

--- script.js
+++ script.js
@@ -1,20 +1,4 @@
 function CheckOnlineStatus(msg) {
-
-    var status = document.getElementById("status");
-    var condition = navigator.onLine ? "ONLINE" : "OFFLINE";
-    var state = document.getElementById("state");
-    state.innerHTML = condition;
-}
-function Pageloaded() {
-    CheckOnlineStatus("load");
-    document.body.addEventListener("offline", function () {
-        CheckOnlineStatus("offline")
-    }, false);
-    document.body.addEventListener("online", function () {
-        CheckOnlineStatus("online")
-    }, false);
-}
-
   var status = document.getElementById("status");
   var condition = navigator.onLine ? "ONLINE" : "OFFLINE";
   var state = document.getElementById("state");
@@ -37,4 +21,3 @@ function Pageloaded() {
     false
   );
 }
-
