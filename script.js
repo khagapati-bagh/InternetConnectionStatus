aca1e73c2e6c2629d02ecb844ff52624d81dba8c
Antony Rafael <depaulacostaantony@gmail.com>
2021-10-26 09:59:14 -0300

    #16 view provider name on screen

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
-function CheckOnlineStatus(msg) {
-  var status = document.getElementById("status");
-  var condition = navigator.onLine ? "ONLINE" : "OFFLINE";
-  var state = document.getElementById("state");
+const getApi = async (url) => {
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
+};
+
+async function CheckOnlineStatus(msg) {
+  let { org } = await getApi("http://ip-api.com/json/");
+  let status = document.getElementById("status");
+  let providerName = document.querySelector(".provider-name");
+  let condition = navigator.onLine ? "ONLINE" : "OFFLINE";
+  let state = document.getElementById("state");
   state.classList.add(condition);
   state.innerHTML = condition;
+  providerName.innerHTML = org;
 }
+
 function Pageloaded() {
   CheckOnlineStatus("load");
   document.body.addEventListener(
